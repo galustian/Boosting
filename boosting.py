f9429f77af50651c804fd39d3a680c738450fc70
galustian <leonard@galustian.com>
2018-04-25 21:58:06 +0200

    add decision-tree-stump part

--- boosting.py
+++ boosting.py
@@ -1,12 +1,12 @@
 import numba as nb
 import numpy as np
 
-spec = [
+spec1 = [
     ('n_classifiers', nb.uint16),
     ('extra_trees', nb.boolean)
 ]
 
-@nb.jitclass(spec)
+@nb.jitclass(spec1)
 class AdaBoostClassifier:
     
     def __init__(self, n_classifiers=50, extra_trees=False):
@@ -24,19 +24,51 @@ class AdaBoostClassifier:
 
         for i in range(n_classifiers):
             stump_classifier = DecisionTreeStump(extra_trees)
-    
-        
+
+
     def predict(self, X):
         pass
 
+spec2 = [
+    ('extra_trees', nb.boolean)
+]
 
+@nb.jitclass()
 class DecisionTreeStump:
 
     def __init__(self, extra_trees=False):
         self.extra_trees = extra_trees
 
+    # Compute the best classifier
     def fit(self, X, X_Weights, Y)
-        pass
+        n_feat = X.shape[1]
+
+        best_feat_i = 0
+        best_feat_size = 0.0  # smaller than feat_size (on the left side): zeros, on the right: ones
+        best_error = 0.0
+        
+        # TODO (maybe): make algorithm more efficient
+        for feat_i in range(n_feat):
+            # Compute Error for each possible tree => choose best stump
+            for n in range(X.shape[0]):
+                n_feat_val = X[n, feat_i]
+                
+                # err_right = 1 - err_left
+                stump_left = X[X[:, feat_i] < n_feat_val]
+                
+                # sum up all weights for misclassified samples
+                error = 0.0
+                for x_i in range(stump_left.shape[0]):
+                    if Y[x_i] != 0:
+                        error += X_Weights[x_i]
+
+                if error < best_error:
+                    best_error = error
+                    best_feat_i = feat_i
+                    best_feat_size = n_feat_val
+        
+        
+
 
     def predict(self, X)
         pass
