fedbd56f5e36a2788b56fbb3d06ac773fb9db0c6
galustian <leonard@galustian.com>
2018-05-01 15:57:59 +0200

    finish decision tree and add new tests

---
+++ decision_tree_regressor.py
@@ -0,0 +1,125 @@
+import numpy as np
+import numba as nb
+
+spec = [
+    ('STEPS', nb.uint32),
+    ('DEPTH', nb.uint8),
+    ('MIN_DATAPOINTS', nb.uint16),
+    # ('structure')
+]
+
+# @nb.jitclass(spec)
+class DecisionTreeRegressor:
+
+    def __init__(self, tree_depth=3, min_datapoints=6):
+        self.STEPS = 25
+        self.DEPTH = tree_depth
+        self.MIN_DATAPOINTS = min_datapoints
+        self.structure = {}
+
+    def fit(self, X, Y):
+        # Give datapoints an ID
+        X = np.c_[np.arange(len(X)), X]
+        self.recurse_split(X, Y, structure=self.structure, depth=1)
+    
+    def recurse_split(self, X_region, Y_region, structure=None, depth=0):
+        if 'END_NODE' in structure: 
+            return
+
+        split = self.get_best_X_split(X_region, Y_region)
+
+        X_left, Y_left = split['X_left'], split['Y_left']
+        X_right, Y_right = split['X_right'], split['Y_right']
+        
+        # TODO maybe variable importance?
+        # best_err = split['best_err']
+
+        structure['feat_i'] = split['feat_i']
+        structure['feat_val'] = split['feat_val']
+
+        # If maximum Depth reached: construct right- and left endnodes
+        if depth == self.DEPTH:
+            structure['left_next'] = {}
+            structure['left_next']['END_NODE'] = True
+            structure['left_next']['prediction'] = Y_left.mean()
+            
+            structure['right_next'] = {}
+            structure['right_next']['END_NODE'] = True
+            structure['right_next']['prediction'] = Y_right.mean()
+    
+            return
+
+        # If left of right less than min. datapoints, construct respective endnode
+        if len(X_left) < self.MIN_DATAPOINTS:
+            structure['left_next'] = {}
+            structure['left_next']['END_NODE'] = True
+            structure['left_next']['prediction'] = Y_left.mean()
+        if len(X_right) < self.MIN_DATAPOINTS:
+            structure['right_next'] = {}
+            structure['right_next']['END_NODE'] = True
+            structure['right_next']['prediction'] = Y_right.mean()
+
+        if 'left_next' not in structure:
+            structure['left_next'] = {}
+        if 'right_next' not in structure:
+            structure['right_next'] = {}
+        
+        self.recurse_split(X_left, Y_left, structure=structure['left_next'], depth=depth+1)
+        self.recurse_split(X_right, Y_right, structure=structure['right_next'], depth=depth+1)
+        
+
+    def get_best_X_split(self, X_region, Y_region):
+        best_feat_i = 0
+        best_feat_val = 0.0
+
+        best_err = 9999999.9
+        
+        for feat_i in range(1, X_region.shape[1]-1):
+            feat_min = X_region[:, feat_i].min()
+            feat_max = X_region[:, feat_i].max()
+            feat_steps = np.linspace(feat_min, feat_max, self.STEPS)
+
+            for feat_step in feat_steps:
+                X_Y = np.c_[X_region, Y_region]
+                
+                XY_left = X_Y[X_region[:, feat_i] < feat_step]
+                XY_right = X_Y[X_region[:, feat_i] >= feat_step]
+
+                region_err = self.get_region_MSE(XY_left[:, -1]) + self.get_region_MSE(XY_right[:, -1])
+
+                if region_err < best_err:
+                    best_err = region_err
+                    best_feat_i = feat_i
+                    best_feat_val = feat_step
+        
+        # Split X and Y based on best splitpoint
+        X_Y = np.c_[X_region, Y_region]
+        
+        best_XY_left = X_Y[X_Y[:, best_feat_i] < best_feat_val]
+        best_XY_right = X_Y[X_Y[:, best_feat_i] >= best_feat_val]
+
+        X_left, Y_left = best_XY_left[:, :-1], best_XY_left[:, -1]
+        X_right, Y_right = best_XY_right[:, :-1], best_XY_right[:, -1]
+        
+        return {'X_left': X_left, 'Y_left': Y_left, 
+                'X_right': X_right, 'Y_right': Y_right, 
+                'feat_i': best_feat_i, 'feat_val': best_feat_val, 'best_err': best_err}
+
+    def get_region_MSE(self, Y):
+        if len(Y) == 0: return 0
+        Y_mean = Y.mean()
+        return np.average(np.square(Y - Y_mean))
+
+
+    def predict(self, X, struct=None):
+        if struct == None:
+            X = np.r_[0, X]
+            struct = self.structure
+        
+        if 'END_NODE' in struct:
+            return struct['prediction']
+            
+        if X[struct['feat_i']] < struct['feat_val']:
+            return self.predict(X, struct=struct['left_next'])
+        else:
+            return self.predict(X, struct=struct['right_next'])
---
+++ decision_tree_regressor_test.py
@@ -0,0 +1,69 @@
+import numpy as np
+from decision_tree_regressor import DecisionTreeRegressor
+
+def test_get_best_X_split():
+    reg = DecisionTreeRegressor(tree_depth=3, min_datapoints=3)
+
+    X_region = np.array([
+        [0, 4.5, 3, 4],
+        [1, 4.55, 0.3, 5],
+        [2, 6.8, 0.102, 6],
+        [3, 6.99, 99, 7],
+        [4, 9.01, 102, 8],
+        [5, 19.01, 4, 7.5],
+    ])
+    Y_region = np.array([2.4, 2.45, 3.57, 4.5, 4.9, 5.5])
+    
+    split = reg.get_best_X_split(X_region, Y_region)
+    
+    # ASSERT
+    X_left = np.array([
+        [0, 4.5, 3, 4],
+        [1, 4.55, 0.3, 5],
+        [2, 6.8, 0.102, 6],
+    ])
+    X_right = np.array([
+        [3, 6.99, 99, 7],
+        [4, 9.01, 102, 8],
+        [5, 19.01, 4, 7.5],
+    ])
+    
+    assert np.array_equiv(split['X_left'], X_left)
+    assert np.array_equiv(split['X_right'], X_right)
+    assert 6.8 <= split['feat_val'] <= 6.99
+
+def test_predict():
+    reg = DecisionTreeRegressor(tree_depth=1, min_datapoints=3)
+    X = np.array([
+        [4.5, 3, 4],
+        [4.55, 0.3, 5],
+        [6.8, 0.102, 6],
+        [6.99, 99, 7],
+        [9.01, 102, 8],
+        [19.01, 4, 7.5],
+    ])
+    Y = np.array([2.4, 2.45, 3.57, 4.5, 4.9, 5.5])
+
+    reg.fit(X, Y)
+
+    assert reg.predict(np.array([4.52, 45, 455.03])) == Y[:3].mean()
+
+    reg = DecisionTreeRegressor(tree_depth=2, min_datapoints=3)
+    X = np.array([
+        [4.5, 3, 4],
+        [4.55, 0.3, 5],
+        [6.8, 0.102, 6],
+        [6.99, 99, 7],
+        [9.01, 112, 8],
+        [19.01, 4, 7.5],
+        [20.01, 120, 0.5],
+        [21.01, 133, 9.5],
+        [18.21, 145, 7.5],
+        [24.01, 150, 1.5],
+        [23.31, 155, 3.5],
+    ])
+    Y = np.array([2.4, 2.45, 3.57, 44.5, 44.9, 45.5, 50.3, 52, 55, 56, 57])
+
+    reg.fit(X, Y)
+
+    assert reg.predict(np.array([18, 165, 4])) == Y[6:].mean() # -3
