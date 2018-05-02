fe9dc2d448eb553b1de8441d7641dfe0eccca4f9
galustian <leonard@galustian.com>
2018-05-02 21:21:24 +0200

    finish gradient-boosting-regressor

--- gradient_boosting_regressor.py
+++ gradient_boosting_regressor.py
@@ -1,16 +1,19 @@
 import numpy as np
 import numba as nb
 from decision_tree_regressor import DecisionTreeRegressor
+#from sklearn.tree import DecisionTreeRegressor
 
 # TODO choose different loss functions
 
 class GradientBoostingRegressor:
 
-    def __init__(self, iterations=50, tree_depth=3, min_tree_region_datapoints=6):
+    def __init__(self, iterations=40, learning_rate=0.1, tree_depth=13, min_tree_region_datapoints=12):
         self.gradient_estimating_trees = []
         self.iterations = iterations
+        self.learning_rate = learning_rate
         self.tree_depth = tree_depth
         self.min_tree_region_datapoints = min_tree_region_datapoints
+        self.Y_mean_vector = None
 
     def fit(self, X, Y):
         # Usually the first estimator (y_hat) is the mean of all Y's
@@ -20,6 +23,7 @@ class GradientBoostingRegressor:
         # Fit regression tree to estimate gradient of loss function (generalizes well on test-data)
         Neg_gradient = self.negative_MSE_gradient(Y, self.Y_mean_vector)
 
+        #grad_estimating_tree = DecisionTreeRegressor(max_depth=self.tree_depth, min_samples_split=self.min_tree_region_datapoints)
         grad_estimating_tree = DecisionTreeRegressor(self.tree_depth, self.min_tree_region_datapoints)
         grad_estimating_tree.fit(X, Neg_gradient)
         
@@ -29,6 +33,7 @@ class GradientBoostingRegressor:
             Prediction = self.predict(X)
             Neg_gradient = self.negative_MSE_gradient(Y, Prediction)
             
+            #grad_estimating_tree = DecisionTreeRegressor(max_depth=self.tree_depth, min_samples_split=self.min_tree_region_datapoints)
             grad_estimating_tree = DecisionTreeRegressor(self.tree_depth, self.min_tree_region_datapoints)
             grad_estimating_tree.fit(X, Neg_gradient)
 
@@ -36,14 +41,14 @@ class GradientBoostingRegressor:
 
     
     def predict(self, X):
-        Prediction = self.Y_mean_vector
+        Prediction = [self.Y_mean_vector[0] for i in range(len(X))]
 
         for reg_tree in self.gradient_estimating_trees:
             Neg_Gradient = reg_tree.predict(X)
-            Prediction += Neg_Gradient
+            Prediction += self.learning_rate * Neg_Gradient
         
         return Prediction
 
-    # residual of y and y_hat
+    # residual of y_hat and y
     def negative_MSE_gradient(self, Y, Y_hat):
-        return -(Y - Y_hat)
+        return -(Y_hat - Y)
--- gradient_boosting_regressor_test.py
+++ gradient_boosting_regressor_test.py
@@ -1,18 +1,33 @@
 import numpy as np
 import pandas as pd
 from gradient_boosting_regressor import GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor as GradientBoostingRegressor2
 
-def gradient_boosting_regressor():
-    reg = GradientBoostingRegressor(iterations=20, tree_depth=3)
+def test_gradient_boosting_regressor():
+    reg = GradientBoostingRegressor(iterations=50, learning_rate=0.065, tree_depth=5)
     
-    df = pd.read_csv('USA_Housing.csv').sample(frac=1, random_state=1)
-    df = df.iloc[:, :-1]
+    df = pd.read_csv('boston.csv').sample(frac=1, random_state=14).iloc[:, 1:]
 
-    X_train, Y_train = df.iloc[:int(len(df) / 2), :-1].as_matrix(), df.iloc[:int(len(df) / 2), -1].as_matrix()
-    X_test, Y_test = df.iloc[int(len(df) / 2):, :-1].as_matrix(), df.iloc[int(len(df) / 2):, -1].as_matrix()
+    train_len = int(len(df) / 2)
 
+    X_train, Y_train = df.iloc[:train_len, :-1].as_matrix(), df.iloc[:train_len, -1].as_matrix()
+    X_test, Y_test = df.iloc[train_len:, :-1].as_matrix(), df.iloc[train_len:, -1].as_matrix()
+    
     reg.fit(X_train, Y_train)
     Y_hat = reg.predict(X_test)
 
+    dec_reg = DecisionTreeRegressor(max_depth=15, min_samples_split=10)
+    dec_reg.fit(X_train, Y_train)
+    Y_hat2 = dec_reg.predict(X_test)
+
+    grad2_reg = GradientBoostingRegressor2(n_estimators=50, learning_rate=0.065, max_depth=5)
+    grad2_reg.fit(X_train, Y_train)
+    Y_hat3 = grad2_reg.predict(X_test)
+    
     # Compute MSE
-    print("MSE:", np.average(np.square(Y_test - Y_hat)))
+    print("Grad-MSE:", np.var(Y_test - Y_hat))
+    print("Dec-MSE:", np.var(Y_test - Y_hat2))
+    print("Grad2-MSE:", np.var(Y_test - Y_hat3))
+
+    assert np.var(Y_test - Y_hat) < np.var(Y_test - Y_hat2)
